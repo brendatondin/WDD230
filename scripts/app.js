ae692a88cf4e7f841bc786e30f9dbe467b8c9942
brendatondin <bentotbrenda@gmail.com>
2023-01-06 17:48:52 -0300

    js for year footer

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
 document.getElementById("placeholder").innerHTML = new Date(document.lastModified);
+
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `@${year} .:|:. Brenda T. Bento .:|:. RJ`;
+document.querySelector("#date2").textContent = fulldate;
