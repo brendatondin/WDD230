46001e68106d56769d5c5b053af590323d7d1fd1
brendatondin <bentotbrenda@gmail.com>
2023-01-09 17:17:31 -0300

    normalize css and js added at copy right

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,3 @@
 document.getElementById("placeholder").innerHTML = new Date(document.lastModified);
 
-const d = new Date();
-const year = d.getFullYear();
-const fulldate = `@${year} .:|:. Brenda T. Bento .:|:. RJ`;
-document.querySelector("#date2").textContent = fulldate;
+document.querySelector("#date2").textContent = `@${new Date().getFullYear()} .:|:. Brenda T. Bento .:|:. RJ`;
