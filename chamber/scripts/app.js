7ee117208f1740163f48456511f281407a7884c1
brendatondin <bentotbrenda@gmail.com>
2023-02-04 19:35:59 -0300

    alert added

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,6 +1,14 @@
 let lastModified = new Date(document.lastModified);
-let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
-let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let fullDate = lastModified.toLocaleString('en-US', {
+	month: "2-digit",
+	day: "2-digit",
+	year: "numeric"
+});
+let time = lastModified.toLocaleString('en-GB', {
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit"
+});
 let dateTime = `Last Modification: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
 
@@ -34,8 +42,18 @@ const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.querySelector("#date2").textContent = fulldate;
 
+// const dayOfWeek = new Date().getDay();
+
+// if (dayOfWeek === 6 || dayOfWeek === 2){
+//     alert('hey')
+// }
+
+if(dayName==='Monday' || dayName==='Tuesday') {
+	alert('🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.')
+}
+
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("primaryNav").classList.toggle("open");
 }
 
 const x = document.getElementById('hamburger-button')
