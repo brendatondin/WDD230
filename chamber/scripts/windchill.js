3e1548ea9f5546b7fc9e12af8c5c365f55e9b29d
brendatondin <bentotbrenda@gmail.com>
2023-02-19 15:53:08 -0300

    discover file and js image and visit created

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,30 @@
-const temp = document.getElementById('temp').textContent
-const windSpeed = document.getElementById('windSpeed').textContent
-const windChill = document.getElementById('windChill')
+// const tempLocation = document.querySelector("#temp");
+// const windSpeedLocation = document.querySelector("#windspeed");
+// const windChill = document.querySelector("#windchill");
 
-function getWindChill(temp, windSpeed) {
-    return 35.74 + (.6215 * temp) - (35.75 * (windSpeed ** .16)) + (.4275 * temp * (windSpeed ** .16))
+
+// const temp = parseFloat(tempLocation.textContent);
+// const windSpeed = parseFloat(windSpeedLocation.textContent);
+
+// let windChillAmount = 35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) +(0.4275 * temp * (windSpeed ** 0.16));
+
+
+// if(temp<=50 && windSpeed > 3.0){
+//     windChill.innerText = `${windChillAmount.toFixed(0)} ºF`;
+// }else{
+//     windChill.innerText = "N/A";
+// }
+
+const temperature = document.getElementById("temperature").textContent;
+const windSpeed = document.getElementById("wind-speed").textContent;
+const windChill = document.getElementById("wind-chill");
+
+function getWindChill(temperature, windSpeed) {
+    return 35.74 + (0.6215 * temperature) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temperature * (windSpeed ** 0.16));
 }
 
-// (temp <= 50 && windSpeed > 3) ? (windChill.textContent = `${parseInt(getWindChill(temp, windSpeed))}°`) : (windChill.textContent = 'N/A')
-
-if (temp <= 50 && windSpeed > 3) {
-    windChill.textContent = `${parseInt(getWindChill(temp, windSpeed))}°`;
+if (temperature <= 50 && windSpeed > 3) {
+    windChill.textContent = `${parseInt(getWindChill(temperature, windSpeed))}°`;
 } else {
     windChill.textContent = "N/A";
 }
